f08ca83379ad6f02f4eee9d1c16cd1f8ff4715c8
ahmedalharth <osamaahmedalharth@gmail.com>
2024-09-03 15:37:27 +0300

    add files

--- SCM APP/app.py
+++ SCM APP/app.py
@@ -18,33 +18,29 @@ st.image("/Users/admin/Data science/Data science EpsilonAI/EpsilonAI-course-ML/F
 
 
 st.title("**SMC App**")
+intro, analysis , rsults = st.tabs(
+    [
+        "Introduction",
+        "Data Analysis",
+        "Results"
+    ]
+)
 
-if "page" not in st.session_state:
-    st.session_state.page = "Introduction"
 
-# Sidebar navigation
-st.sidebar.title("Navigation")
-page = st.sidebar.selectbox("Select a page", ["Introduction", "Data Analysis", "Results"])
-
-# Update session state with the selected page
-if page:
-    st.session_state.page = page
-
-# Display selected page content
-if st.session_state.page == "Introduction":
+with intro:
     st.title("Introduction")
     st.subheader("What is Supply Chain Management?")
     st.write("""Supply Chain Management (SCM) involves the oversight and management of the flow of goods and services from raw material suppliers to end consumers. 
-                It encompasses the planning and management of all activities involved in sourcing, procurement, conversion, and logistics.""")
+                    It encompasses the planning and management of all activities involved in sourcing, procurement, conversion, and logistics.""")
 
     st.subheader("Problem Statment")
     st.write("""In today's global economy, supply chain disruptions pose a significant threat to businesses, affecting their ability to deliver products on time and maintain profitability. The increasing complexity of supply chains, coupled with the growing emphasis on environmental sustainability, requires companies to carefully balance efficiency, cost, and risk.
-    Despite the wealth of data available, many companies struggle to effectively assess and manage supply chain risk. Traditional risk management approaches often fail to account for the dynamic nature of global supply chains and the diverse factors influencing risk. Therefore, there is a need for a data-driven approach to predict and mitigate supply chain risk, considering both SCM practices and green logistics initiatives.""")
-    
+        Despite the wealth of data available, many companies struggle to effectively assess and manage supply chain risk. Traditional risk management approaches often fail to account for the dynamic nature of global supply chains and the diverse factors influencing risk. Therefore, there is a need for a data-driven approach to predict and mitigate supply chain risk, considering both SCM practices and green logistics initiatives.""")
+        
     st.subheader("The Data")
     st.write("""SCM Dataset with Green Logistics
-             The dataset titled "SCM Dataset with Green Logistics" comprises a comprehensive collection of data from various companies, detailing their supply chain management (SCM) practices, performance metrics, and green logistics initiatives.
-              \n* Data source [Kaggle](https://www.kaggle.com/datasets/lastman0800/supply-chain-management).""")
+                The dataset titled "SCM Dataset with Green Logistics" comprises a comprehensive collection of data from various companies, detailing their supply chain management (SCM) practices, performance metrics, and green logistics initiatives.
+                \n* Data source [Kaggle](https://www.kaggle.com/datasets/lastman0800/supply-chain-management).""")
     
     
 
@@ -65,10 +61,10 @@ if st.session_state.page == "Introduction":
 #         st.write("This is outside the container")
 
 
-elif st.session_state.page == "Data Analysis":
-    st.title("Data Analysis")
-    st.write("This is the data analysis page.")
+    # elif st.session_state.page == "Data Analysis":
+    #     st.title("Data Analysis")
+    #     st.write("This is the data analysis page.")
 
-elif st.session_state.page == "Results":
-    st.title("Results")
-    st.write("This is the results page.")
+    # elif st.session_state.page == "Results":
+    #     st.title("Results")
+    #     st.write("This is the results page.")
