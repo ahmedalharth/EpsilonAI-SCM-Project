cadc208f0cc753af22c85c7e512a2be1a6f191df
ahmedalharth <osamaahmedalharth@gmail.com>
2024-09-06 18:24:23 +0300

    add files

--- app.py
+++ app.py
@@ -29,18 +29,23 @@ train_path = '/Users/admin/Data science/Data science EpsilonAI/EpsilonAI-course-
 test_path = '/Users/admin/Data science/Data science EpsilonAI/EpsilonAI-course-ML/Final Project/SCM/SCM APP/Data/Test.csv'
 # images
 # icon_image = Image.open('/Users/admin/Data science/Data science EpsilonAI/EpsilonAI-course-ML/Final Project/SCM/SCM APP/Images/Postgraduate-Diploma-in-Procurement-and-Supply-Chain-Management-Course-Objectives.jpg')
-model_process = Image.open('/Images/SMC Model-building-process  copy.png')
+model_process = Image.open('https://github.com/ahmedalharth/EpsilonAI-SCM-Project/blob/main/SCM%20APP/Images/SMC%20Model-building-process%20%20copy.png')
+
 # SCM_image = Image.open('/Users/admin/Data science/Data science EpsilonAI/EpsilonAI-course-ML/Final Project/SCM/SCM APP/Images/Supply-Chain-Management-benefits.png')
 # Load the pre-trained XGBoost model
+
 model_path = "/Users/admin/Data science/Data science EpsilonAI/EpsilonAI-course-ML/Final Project/SCM/SCM APP/Models/Xgb_pipeline.pkl"  # Replace with your model file path
 with open(model_path, 'rb') as file:
     model = pickle.load(file)
 lb_path = "/Users/admin/Data science/Data science EpsilonAI/EpsilonAI-course-ML/Final Project/SCM/SCM APP/Models/label_encoders.pkl"
+
 # Load fitted LabelEncoders for categorical features
+
 with open(lb_path, 'rb') as file:
         label_encoders = pickle.load(file)
 
 # read the data 
+
 Old_data = pd.read_excel( primary_data, sheet_name='Sheet1')
 Clean_df = pd.read_csv(clean_data_path)
 Train = pd.read_csv(train_path)
